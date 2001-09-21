43e5d92fdad388893089684421289c77ff6fa97c
parser <parser>
2001-09-21 15:40:55 +0000

    Initial revision

---
+++ oracle/config_fixed.h
@@ -0,0 +1,18 @@
+/** @file
+	Parser PgSQL driver: Configure file for autoconf-disabled platforms.
+
+	Copyright (c) 2001 ArtLebedev Group (http://www.artlebedev.com)
+
+	Author: Alexander Petrosyan <paf@design.ru> (http://design.ru/paf)
+
+	$Id: config_fixed.h,v 1.1 2001/09/21 15:43:55 parser Exp $
+
+
+	tested only with MS Visual C++ V6
+*/
+
+#define HAVE_STDIO_H
+#define HAVE_STDLIB_H
+#define HAVE_STRING_H
+#define HAVE_SETJMP_H
+#define HAVE_CTYPE_H
