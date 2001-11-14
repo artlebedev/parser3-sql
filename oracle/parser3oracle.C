bf543ba59166f4f426d2ee37855576c5fc7f30d5
Alexander Petrossian <paf@design.ru>
2001-11-14 09:46:31 +0000

    removed return [enabled actuall disconnect]

--- oracle/parser3oracle.C
+++ oracle/parser3oracle.C
@@ -7,7 +7,7 @@
 
 	2001.07.30 using Oracle 8.1.6 [@test tested with Oracle 7.x.x]
 */
-static const char *RCSId="$Id: parser3oracle.C,v 1.11 2001/11/14 09:40:13 paf Exp $"; 
+static const char *RCSId="$Id: parser3oracle.C,v 1.12 2001/11/14 09:46:31 paf Exp $"; 
 
 #include "config_includes.h"
 
@@ -317,12 +317,7 @@ public:
 		// return created connection
 		*(OracleSQL_connection_struct **)connection=&cs;
 	}
-	/// @test remove return
 	void disconnect(void *connection) {
-
-		return;
-
-
 	    OracleSQL_connection_struct &cs=*(OracleSQL_connection_struct *)connection;
 		// Terminate a user session
 		OCISessionEnd(
@@ -606,7 +601,7 @@ private: // private funcs
 	void fetch_table(SQL_Driver_services& services, OracleSQL_connection_struct &cs, 
 		OCIStmt *stmthp, unsigned long offset, unsigned long limit, 
 		SQL_Driver_query_event_handlers& handlers) {
-/*
+
 		ub4 prefetch_rows=100;
 		check(services, cs, "AttrSet prefetch-rows", OCIAttrSet( 
 			(dvoid *)stmthp, (ub4)OCI_HTYPE_STMT, 
@@ -618,7 +613,6 @@ private: // private funcs
 			(dvoid *)stmthp, (ub4)OCI_HTYPE_STMT, 
 			(dvoid *)&prefetch_mem_size, (ub4)0, 
 			(ub4)OCI_ATTR_PREFETCH_MEMORY, (OCIError *)cs.errhp));
-*/
 
 		OCIParam          *mypard;
 		ub2                    dtype;
