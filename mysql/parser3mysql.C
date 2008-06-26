c538dceb6bcdaf0d0b1689606ce07766bf7b4fee
Michael Petrushin <misha@design.ru>
2008-06-26 14:30:48 +0000

    - use new API and new version (10.0) - $.limit(0) fixed - transcode server error messaged if ClientCharset option was specified - less calls to transcode method if ClientCharset specified but it's equal to request:charset

--- mysql/parser3mysql.C
+++ mysql/parser3mysql.C
@@ -10,7 +10,7 @@
 	2001.11.06 numrows on "HP-UX istok1 B.11.00 A 9000/869 448594332 two-user license"
 		3.23.42 & 4.0.0.alfa never worked, both subst & .sl version returned 0
 */
-static const char *RCSId="$Id: parser3mysql.C,v 1.32 2008/06/24 17:21:10 misha Exp $"; 
+static const char *RCSId="$Id: parser3mysql.C,v 1.33 2008/06/26 14:30:48 misha Exp $"; 
 
 #include "config_includes.h"
 
@@ -66,7 +66,7 @@ struct Connection {
 	SQL_Driver_services* services;
 
 	MYSQL* handle;
-	const char* cstrClientCharset;
+	const char* client_charset;
 	bool autocommit;
 	bool multi_statements;
 };
@@ -87,31 +87,33 @@ public:
 
 	/// get api version
 	int api_version() { return SQL_DRIVER_API_VERSION; }
+
 	/// initialize driver by loading sql dynamic link library
 	const char *initialize(char *dlopen_file_spec) {
 		return dlopen_file_spec?
 			dlink(dlopen_file_spec):"client library column is empty";
 	}
+
 	/**	connect
 		@param url
 			format: @b user:pass@host[:port]|[/unix/socket]/database?
+				charset=value&	// transcode by server with command 'SET CHARACTER SET value'
 				ClientCharset=charset&	// transcode by parser
-				charset=cp1251_koi8&	// transcode by server with 'set CHARACTER SET xyz'
 				timeout=3&
 				compress=0&
 				named_pipe=1&
-				autocommit=1
-			3.23.22b
-				Currently the only option for @b character_set_name is cp1251_koi8.
-				WARNING: must be used only to connect, for buffer doesn't live long
-			4.1+ accept not only 'cp1251_koi8' but 'cp1251', 'utf8' and much more
-				it can be usable for transcoding using sql server
+				autocommit=1&
+				multi_statements=0	// allow more then one statement in one query
+			3.x, 4.0 
+				only option for charset is cp1251_koi8.
+			4.1+ accept not 'cp1251_koi8' but 'cp1251', 'utf8' and much more
+				it is usable for transcoding using sql server
 	*/
 	void connect(
-		char *url, 
-		SQL_Driver_services& services, 
-		void **connection_ref ///< output: Connection*
-		) {
+				char *url, 
+				SQL_Driver_services& services, 
+				void **connection_ref ///< output: Connection*
+	){
 		char *user=url;
 		char *s=rsplit(user, '@');
 		char *host=0;
@@ -129,25 +131,25 @@ public:
 		int port=port_cstr?strtol(port_cstr, &error_pos, 0):0;
 		char *options=lsplit(db, '?');
 
-		char *cstrBackwardCompAskServerToTranscode=0;
+		char *charset=0;
 
-		Connection& connection=*(Connection  *)services.malloc(sizeof(Connection));
+		Connection& connection=*(Connection *)services.malloc(sizeof(Connection));
+		*connection_ref=&connection;
 		connection.services=&services;
 		connection.handle=mysql_init(NULL);
-		connection.cstrClientCharset=0;	
+		connection.client_charset=0;	
 		connection.autocommit=true;
 		connection.multi_statements=false;
-		*connection_ref=&connection;
 
 		while(options){
 			if(char *key=lsplit(&options, '&')){
 				if(*key){
 					if(char *value=lsplit(key, '=')){
-						if(strcmp(key, "ClientCharset" )==0){ // transcoding with parser
+						if(strcmp(key, "ClientCharset")==0){ // transcoding with parser
 							toupper_str(value, value, strlen(value));
-							connection.cstrClientCharset=value;
+							connection.client_charset=value;
 						} else if(strcasecmp(key, "charset")==0){ // transcoding with server
-							cstrBackwardCompAskServerToTranscode=value;
+							charset=value;
 						} else if(strcasecmp(key, "timeout")==0){
 							unsigned int timeout=(unsigned int)atoi(value);
 							if(mysql_options(connection.handle, MYSQL_OPT_CONNECT_TIMEOUT, (const char *)&timeout)!=0)
@@ -158,7 +160,7 @@ public:
 									services._throw(mysql_error(connection.handle));
 						} else if(strcasecmp(key, "named_pipe")==0){
 							if(atoi(value))
-								if(mysql_options(connection.handle, MYSQL_OPT_NAMED_PIPE , 0)!=0)
+								if(mysql_options(connection.handle, MYSQL_OPT_NAMED_PIPE, 0)!=0)
 									services._throw(mysql_error(connection.handle));
 						} else if(strcasecmp(key, "multi_statements")==0) {
 							if(atoi(value)!=0)
@@ -174,53 +176,47 @@ public:
 			}
 		}
 
-		if(!mysql_real_connect(connection.handle, 
-			host, user, pwd, db, port?port:MYSQL_PORT, unix_socket, (connection.multi_statements) ? CLIENT_MULTI_STATEMENTS : CLIENT_MULTI_RESULTS))
+		if(!mysql_real_connect(
+					connection.handle, 
+					host, user, pwd, db,
+					port?port:MYSQL_PORT,
+					unix_socket,
+					(connection.multi_statements) ? CLIENT_MULTI_STATEMENTS : CLIENT_MULTI_RESULTS
+				)
+		){
 			services._throw(mysql_error(connection.handle));
+		}
 
-		if(cstrBackwardCompAskServerToTranscode) {
-			// set charset
+		if(charset){
 			char statement[MAX_STRING]="SET CHARACTER SET ";
-			strncat(statement, cstrBackwardCompAskServerToTranscode, MAX_STRING);
-			
-			exec(connection, statement);
+			strncat(statement, charset, MAX_STRING);
+			_exec(connection, statement);
 		}
 
 		if(!connection.autocommit)
-			exec(connection, "SET AUTOCOMMIT=0");
-	}
-
-	void exec(Connection& connection, const char* statement) {
-		if(mysql_query(connection.handle, statement)) 
-			connection.services->_throw(mysql_error(connection.handle));
-		(*mysql_store_result)(connection.handle); // throw out the result [don't need but must call]
+			_exec(connection, "SET AUTOCOMMIT=0");
 	}
 
 	void disconnect(void *aconnection) {
 		Connection& connection=*static_cast<Connection*>(aconnection);
-
 		mysql_close(connection.handle);
 		connection.handle=0;
 	}
 
 	void commit(void *aconnection) {
-		//_asm int 3;
 		Connection& connection=*static_cast<Connection*>(aconnection);
-
 		if(!connection.autocommit)
-			exec(connection, "COMMIT");
+			_exec(connection, "COMMIT");
 	}
 
 	void rollback(void *aconnection) {
 		Connection& connection=*static_cast<Connection*>(aconnection);
-
 		if(!connection.autocommit)
-			exec(connection, "ROLLBACK");
+			_exec(connection, "ROLLBACK");
 	}
 
 	bool ping(void *aconnection) {
 		Connection& connection=*static_cast<Connection*>(aconnection);
-
 		return mysql_ping(connection.handle)==0;
 	}
 
@@ -236,48 +232,51 @@ public:
 		mysql_escape_string(result, from, length);
 		return result;
 	}
+
 	void query(void *aconnection, 
-		const char *astatement, 
-		size_t placeholders_count, Placeholder* placeholders, 
-		unsigned long offset, unsigned long limit,
-		SQL_Driver_query_event_handlers& handlers) {
+				const char *astatement, 
+				size_t placeholders_count, Placeholder* placeholders, 
+				unsigned long offset, unsigned long limit,
+				SQL_Driver_query_event_handlers& handlers
+	){
 		Connection& connection=*static_cast<Connection*>(aconnection);
 		SQL_Driver_services& services=*connection.services;
-		const char* cstrClientCharset=connection.cstrClientCharset;
 		MYSQL_RES *res=NULL;
 
 		if(placeholders_count>0)
 			services._throw("bind variables not supported (yet)");
 
-		// transcode from $request:charset to connect-string?client_charset
-		if(cstrClientCharset) {
-			size_t transcoded_statement_size;
-			services.transcode(astatement, strlen(astatement),
-				astatement, transcoded_statement_size,
+		bool transcode_needed=_transcode_required(connection);
+
+		// transcode query from $request:charset to ?ClientCharset
+		if(transcode_needed){
+			size_t length=strlen(astatement);
+			services.transcode(astatement, length,
+				astatement, length,
 				services.request_charset(),
-				cstrClientCharset);
+				connection.client_charset);
 		}
 
 		const char *statement;
-		if(offset || limit) {
+		if(offset || limit!=SQL_NO_LIMIT){
 			size_t statement_size=strlen(astatement);
 			char *statement_limited=(char *)services.malloc_atomic(
-				statement_size+MAX_NUMBER*2+8/* limit #,#*/+1);
+				statement_size+MAX_NUMBER*2+8/* LIMIT #,#*/+1);
 			char *cur=statement_limited;
 			memcpy(cur, astatement, statement_size); cur+=statement_size;
-			cur+=sprintf(cur, " limit ");
+			cur+=sprintf(cur, " LIMIT ");
 			if(offset)
 				cur+=snprintf(cur, MAX_NUMBER+1, "%u,", offset);
-			if(limit)
+			if(limit!=SQL_NO_LIMIT)
 				cur+=snprintf(cur, MAX_NUMBER, "%u", limit);
 			statement=statement_limited;
 		} else
 			statement=astatement;
 
 		if(mysql_query(connection.handle, statement)) 
-			services._throw(mysql_error(connection.handle));
+			_throw(connection, mysql_error(connection.handle));
 		if(!(res=mysql_store_result(connection.handle)) && mysql_field_count(connection.handle)) 
-			services._throw(mysql_error(connection.handle));
+			_throw(connection, mysql_error(connection.handle));
 		if(!res) // empty result: insert|delete|update|...
 			return;
 		
@@ -285,7 +284,7 @@ public:
 		if(!column_count) // old client
 			column_count=mysql_field_count(connection.handle);
 
-		if(!column_count) {
+		if(!column_count){
 			mysql_free_result(res);
 			services._throw("result contains no columns");
 		}
@@ -299,17 +298,17 @@ public:
 		}
 
 		for(int i=0; i<column_count; i++){
-			if(MYSQL_FIELD *field=mysql_fetch_field(res)) {
+			if(MYSQL_FIELD *field=mysql_fetch_field(res)){
 				size_t length=strlen(field->name);
 				char* strm=(char*)services.malloc_atomic(length+1);
 				memcpy(strm, field->name, length+1);
 				const char* str=strm;
 
-				// transcode to $request:charset from connect-string?client_charset
-				if(cstrClientCharset) {
+				// transcode column name from ?ClientCharset to $request:charset 
+				if(transcode_needed){
 					services.transcode(str, length,
 						str, length,
-						cstrClientCharset,
+						connection.client_charset,
 						services.request_charset());
 				}
 				
@@ -324,23 +323,23 @@ public:
 
 		CHECK(handlers.before_rows(sql_error));
 		
-  		while(MYSQL_ROW mysql_row=mysql_fetch_row(res)) {
-  			CHECK(handlers.add_row(sql_error));
-  			unsigned long *lengths=mysql_fetch_lengths(res);
-  			for(int i=0; i<column_count; i++){
-  				size_t length=(size_t)lengths[i];
+		while(MYSQL_ROW mysql_row=mysql_fetch_row(res)){
+			CHECK(handlers.add_row(sql_error));
+			unsigned long *lengths=mysql_fetch_lengths(res);
+			for(int i=0; i<column_count; i++){
+				size_t length=(size_t)lengths[i];
 				const char* str;
-  				if(length) {
-  					char *strm=(char*)services.malloc_atomic(length+1);
-  					memcpy(strm, mysql_row[i], length);
+				if(length){
+					char *strm=(char*)services.malloc_atomic(length+1);
+					memcpy(strm, mysql_row[i], length);
 					strm[length]=0;
 					str=strm;
 
-					// transcode to $request:charset from connect-string?client_charset
-					if(cstrClientCharset)
+					// transcode cell value from ?ClientCharset to $request:charset
+					if(transcode_needed)
 						services.transcode(str, length,
 							str, length,
-							cstrClientCharset,
+							connection.client_charset,
 							services.request_charset());
 				} else
 					str=0;
@@ -353,37 +352,59 @@ cleanup:
 			services._throw(sql_error);
 	}
 
+private:
+	void _exec(Connection& connection, const char* statement) {
+		if(mysql_query(connection.handle, statement)) 
+			_throw(connection, mysql_error(connection.handle));
+		(*mysql_store_result)(connection.handle); // throw out the result [don't need but must call]
+	}
+
+	void _throw(Connection& connection, const char* aerr_msg) {
+		size_t length=strlen(aerr_msg);
+		if(length && _transcode_required(connection)) {
+			connection.services->transcode(aerr_msg, length,
+				aerr_msg, length,
+				connection.client_charset,
+				connection.services->request_charset());
+		}
+		connection.services->_throw(aerr_msg);
+	}
+
+	bool _transcode_required(Connection& connection){
+		return (connection.client_charset && strcmp(connection.client_charset, connection.services->request_charset())!=0);
+	}
+
 private: // mysql client library funcs
 
-	typedef MYSQL* (STDCALL *t_mysql_init)(MYSQL *); 	t_mysql_init mysql_init;
+	typedef MYSQL*	(STDCALL *t_mysql_init)(MYSQL *); 	t_mysql_init mysql_init;
 	
-	typedef void (STDCALL *t_mysql_server_end)(); 	t_mysql_server_end mysql_server_end;
+	typedef void	(STDCALL *t_mysql_server_end)(); 	t_mysql_server_end mysql_server_end;
 
-	typedef int (STDCALL *t_mysql_options)(MYSQL *mysql, enum mysql_option option, const char *arg); t_mysql_options mysql_options;
+	typedef int		(STDCALL *t_mysql_options)(MYSQL *mysql, enum mysql_option option, const char *arg); t_mysql_options mysql_options;
 	
 	typedef MYSQL_RES* (STDCALL *t_mysql_store_result)(MYSQL *); t_mysql_store_result mysql_store_result;
 	
 	typedef int		(STDCALL *t_mysql_query)(MYSQL *, const char *q); t_mysql_query mysql_query;
 	
-	typedef char * (STDCALL *t_mysql_error)(MYSQL *); t_mysql_error mysql_error;
+	typedef char*	(STDCALL *t_mysql_error)(MYSQL *); t_mysql_error mysql_error;
 	static char* STDCALL subst_mysql_error(MYSQL *mysql) { return (mysql)->net.last_error; }
 
-	typedef MYSQL*		(STDCALL *t_mysql_real_connect)(MYSQL *, const char *host,
-					   const char *user,
-					   const char *passwd,
-					   const char *db,
-					   unsigned int port,
-					   const char *unix_socket,
-					   unsigned int clientflag); t_mysql_real_connect mysql_real_connect;
+	typedef MYSQL*	(STDCALL *t_mysql_real_connect)(MYSQL *, const char *host,
+						const char *user,
+						const char *passwd,
+						const char *db,
+						unsigned int port,
+						const char *unix_socket,
+						unsigned int clientflag); t_mysql_real_connect mysql_real_connect;
 
-	typedef void		(STDCALL *t_mysql_close)(MYSQL *); t_mysql_close mysql_close;
+	typedef void	(STDCALL *t_mysql_close)(MYSQL *); t_mysql_close mysql_close;
 	
 	typedef int		(STDCALL *t_mysql_ping)(MYSQL *); t_mysql_ping mysql_ping;
 	
 	typedef unsigned long	(STDCALL *t_mysql_escape_string)(char *to,const char *from,
-					    unsigned long from_length); t_mysql_escape_string mysql_escape_string;
+						unsigned long from_length); t_mysql_escape_string mysql_escape_string;
 	
-	typedef void		(STDCALL *t_mysql_free_result)(MYSQL_RES *result); t_mysql_free_result mysql_free_result;
+	typedef void	(STDCALL *t_mysql_free_result)(MYSQL_RES *result); t_mysql_free_result mysql_free_result;
 	
 	typedef unsigned long* (STDCALL *t_mysql_fetch_lengths)(MYSQL_RES *result); t_mysql_fetch_lengths mysql_fetch_lengths;
 	
@@ -391,22 +412,23 @@ private: // mysql client library funcs
 	
 	typedef MYSQL_FIELD*	(STDCALL *t_mysql_fetch_field)(MYSQL_RES *result); t_mysql_fetch_field mysql_fetch_field;
 	
-	typedef unsigned int (STDCALL *t_mysql_num_fields)(MYSQL_RES *); t_mysql_num_fields mysql_num_fields;
-	static unsigned int STDCALL subst_mysql_num_fields(MYSQL_RES *res) { return res->field_count; }
+	typedef unsigned int	(STDCALL *t_mysql_num_fields)(MYSQL_RES *); t_mysql_num_fields mysql_num_fields;
+	typedef unsigned int	(STDCALL *t_mysql_field_count)(MYSQL *); t_mysql_field_count mysql_field_count;
 
-	typedef unsigned int (STDCALL *t_mysql_field_count)(MYSQL *); t_mysql_field_count mysql_field_count;
-	static unsigned int STDCALL subst_mysql_field_count(MYSQL *mysql) { return mysql->field_count; }
+	static unsigned int	STDCALL subst_mysql_num_fields(MYSQL_RES *res) { return res->field_count; }
+	static unsigned int	STDCALL subst_mysql_field_count(MYSQL *mysql) { return mysql->field_count; }
 
 private: // mysql client library funcs linking
 
 	const char *dlink(const char *dlopen_file_spec) {
 		if(lt_dlinit())
 			return lt_dlerror();
-        lt_dlhandle handle=lt_dlopen(dlopen_file_spec);
-        if (!handle) {
-            if(const char* result=lt_dlerror())
-            	return result;
 
+		lt_dlhandle handle=lt_dlopen(dlopen_file_spec);
+
+		if(!handle){
+			if(const char* result=lt_dlerror())
+				return result;
 			return "can not open the dynamic link module";
 		}
 
